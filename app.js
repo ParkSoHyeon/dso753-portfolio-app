149f412f6ff4e2165bffc0b1a4a4d2e140150429
parksohyeon <dso753@elinmedia.com>
2020-10-30 18:53:06 +0900

    1. port 수정 2. work 코드잇 추가

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 var path = require('path');
-const port = 3000;
+const port = (process.env.PORT || 3000);
 
 app.use('/static', express.static(path.join(__dirname + '/static')));
 
